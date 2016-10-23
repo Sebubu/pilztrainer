f17b99079feacbe58f5333d879b85e1c4babd5d0
Severin Bühler <sevi_buehler@hotmail.com>
2016-10-23 13:59:36 +0200

    added dense train file

--- train.py
+++ train.py
@@ -1,23 +1,35 @@
 from keras.applications.resnet50 import ResNet50
-from keras.preprocessing import image
-from keras.applications.vgg16 import preprocess_input
-import numpy as np
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import optimizers
+from keras.models import Sequential
+from FeatureLoad import load_dataset
+from keras.layers import Input
 
 
-model = ResNet50(include_top=False, weights='imagenet', input_tensor=None)
+model = ResNet50(include_top=False, weights='imagenet', input_tensor=Input(shape=(3, 224, 224)))
 print("loaded Resnet")
+print(model.output_shape[1:])
 
 train_data_dir = '../pilz-scrapper/target/train'
-validation_data_dir = '../pilz-scrapper/target/test'
+test_data_dir = '../pilz-scrapper/target/test'
 
-def load_image(path="7.jpg"):
-    img = image.load_img(path, target_size=(224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    return x
+#train_x, train_y = load_dataset(test_data_dir)
+#test_x, test_y = load_dataset(test_data_dir)
+train_x, train_y = load_dataset("tests/train")
+test_x, test_y = load_dataset("tests/train")
+nb_categories = 2#210
 
-x = load_image()
-print("Loaded image")
-features = model.predict(x)
-print(features)
+top_model = Sequential()
+top_model.add(Flatten(input_shape=model.output_shape[1:]))
+top_model.add(Dense(256, activation='relu'))
+top_model.add(Dropout(0.5))
+top_model.add(Dense(nb_categories, activation='sigmoid'))
+
+
+top_model.compile(loss='categorical_crossentropy',
+              optimizer='adadelta',
+              metrics=['accuracy'])
+print("Compiled")
+
+top_model.fit(train_x, train_y, batch_size=32, nb_epoch=200,
+          verbose=1, validation_data=(test_x, test_y))
