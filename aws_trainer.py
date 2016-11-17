f06aa4f456baf6a12747d58984be1f5632864726
Severin Bühler <sevi_buehler@hotmail.com>
2016-11-17 16:48:50 +0100

    print epoche

--- aws_trainer.py
+++ aws_trainer.py
@@ -13,6 +13,7 @@ from datetime import datetime
 resnet = ResNet50(include_top=False, weights='imagenet', input_tensor=Input(shape=(3, 224, 224)))
 print("loaded Resnet")
 
+batch_size = 128
 train_data_dir = '/home/severin/PycharmProjects/pilztrainer/mushroom_dataset/train'
 test_data_dir = '/home/severin/PycharmProjects/pilztrainer/mushroom_dataset/test'
 image_size = (224,224)
@@ -33,13 +34,13 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 train_generator = train_datagen.flow_from_directory(
         train_data_dir,
         target_size=image_size,
-        batch_size=128,
+        batch_size=batch_size,
         class_mode='categorical')
 
 validation_generator = test_datagen.flow_from_directory(
         test_data_dir,
         target_size=image_size,
-        batch_size=128,
+        batch_size=batch_size,
         class_mode='categorical')
 
 class_dictionary = validation_generator.class_indices
@@ -63,10 +64,22 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 print("Compiled")
 
+def printen(titel, result):
+    loss = result[0]
+    acc = result[1]
+    print("\t" + titel + " loss " + str(loss) + ", acc " + str(acc))
+
+loss = 100
 for i in range(0, 500):
     print("Epoche " + str(i))
     x_train, y_train = train_generator.next()
     x_test, y_test = validation_generator.next()
-    print('\ttrain ' + str(model.train_on_batch(x_train, y_train)))
-    print('\ttest' + str(model.test_on_batch(x_test, y_test)))
-    model.save_weights('my_model_weights' + str(i) + '.hdf5')
+    train_results = model.train_on_batch(x_train, y_train)
+    printen("train", train_results)
+    test = model.test_on_batch(x_test, y_test)
+    printen("test", test)
+    test_loss = test[0]
+    if loss > test_loss:
+        print("\tsave model...")
+        loss = test_loss
+        model.save_weights('weights/weights' + str(i) + 'l' + str(test_loss) +'.hdf5')
