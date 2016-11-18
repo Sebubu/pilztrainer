1c8b7b2e28b4d822c82f9e3a80e58635d04fb189
Severin Bühler <sevi_buehler@hotmail.com>
2016-11-18 11:14:59 +0100

    ajusted

--- aws_trainer.py
+++ aws_trainer.py
@@ -5,16 +5,31 @@ from keras.layers.advanced_activations import LeakyReLU
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 from datetime import datetime
+import os
+import pwd
+
+
+def get_username():
+    return pwd.getpwuid(os.getuid())[0]
 
 
 resnet = ResNet50(include_top=False, weights='imagenet', input_tensor=Input(shape=(3, 224, 224)))
+#whole_resnet = ResNet50(include_top=False, weights=None, input_tensor=Input(shape=(3, 224, 224)))
+#resnet = Model(input=whole_resnet.input, output=whole_resnet .get_layer('activation_46').output)
 print("loaded Resnet")
 
-batch_size = 128
-train_data_dir = '/home/ubuntu/mushroom_dataset/train'
-test_data_dir = '/home/ubuntu/mushroom_dataset/test'
-image_size = (224,224)
-shift=0.2
+batch_size = 256
+test_batch_size = 64
+
+if get_username() == 'severin':
+    train_data_dir = '/home/severin/PycharmProjects/pilztrainer/mushroom_dataset/train'
+    test_data_dir = '/home/severin/PycharmProjects/pilztrainer/mushroom_dataset/test'
+else:
+    train_data_dir = '/home/ubuntu/mushroom_dataset/train'
+    test_data_dir = '/home/ubuntu/mushroom_dataset/test'
+
+image_size = (224, 224)
+shift = 0.2
 train_datagen = ImageDataGenerator(
     rescale=1. / 255,
     shear_range=0.2,
@@ -26,27 +41,31 @@ train_datagen = ImageDataGenerator(
     height_shift_range=shift
 )
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
-        train_data_dir,
-        target_size=image_size,
-        batch_size=batch_size,
-        class_mode='sparse')
+    train_data_dir,
+    target_size=image_size,
+    batch_size=batch_size,
+    class_mode='categorical')
 
 validation_generator = test_datagen.flow_from_directory(
-        test_data_dir,
-        target_size=image_size,
-        batch_size=batch_size,
-        class_mode='sparse')
+    test_data_dir,
+    target_size=image_size,
+    batch_size=test_batch_size,
+    class_mode='categorical')
 
 class_dictionary = validation_generator.class_indices
-print(class_dictionary)
+#print(class_dictionary)
 
 nb_categories = 1510
 
 x = resnet.output
 x = GlobalAveragePooling2D()(x)
+x = Dense(2048)(x)
+x = LeakyReLU()(x)
+x = Dropout(0.5)(x)
 x = Dense(2048)(x)
 x = LeakyReLU()(x)
 x = Dropout(0.5)(x)
@@ -56,11 +75,14 @@ model = Model(input=resnet.input, output=predictions)
 for layer in resnet.layers:
     layer.trainable = False
 
-from keras.optimizers import Adadelta
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer=Adadelta(lr=0.1),
+
+
+from keras.optimizers import RMSprop
+model.compile(loss='categorical_crossentropy',
+              optimizer='adadelta',
               metrics=['accuracy'])
 print("Compiled")
+
 
 def printen(titel, result):
     loss = result[0]
@@ -69,18 +91,23 @@ def printen(titel, result):
 
 loss = 100
 for i in range(0, 500):
-    print("Epoche " + str(i) + " " + str(datetime.now()))
+    print()
+    start = datetime.now()
+    print("Epoche " + str(i))
     x_train, y_train = train_generator.next()
     x_test, y_test = validation_generator.next()
-    print(x_train.shape)
+
     train_results = model.train_on_batch(x_train, y_train)
     printen("train", train_results)
 
     test = model.test_on_batch(x_test, y_test)
     printen("test", test)
+    end = datetime.now()
+    eta = end-start
+    print("\t" + str(eta.seconds) + " seconds")
 
     test_loss = test[0]
     if loss > test_loss:
         print("\tsave model...")
         loss = test_loss
-        model.save_weights('weights/weights' + str(i) + 'l' + str(test_loss) +'.hdf5')
+        model.save_weights('weights/weights' + str(i) + 'l' + str(test_loss) + '.hdf5')
