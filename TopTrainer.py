4813eeb0f535758476b12b4e65185cff283c34b9
Severin Bühler <sevi_buehler@hotmail.com>
2016-11-19 15:30:03 +0100

    added dense

--- TopTrainer.py
+++ TopTrainer.py
@@ -4,6 +4,7 @@ from keras.layers import Input
 from keras.layers.advanced_activations import LeakyReLU
 from keras.models import Model
 from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.metrics import categorical_accuracy
 
 x_batch,y_batch = load()
 x_test,y_test = load('bottlenecktest/')
@@ -21,6 +22,8 @@ nb_categories = 1510
 
 inputs = Input(x_batch.shape[1:])
 x = GlobalAveragePooling2D()(inputs)
+x = Dense(512)(x)
+x = LeakyReLU()(x)
 x = Dropout(0.5)(x)
 x = Dense(512)(x)
 x = LeakyReLU()(x)
@@ -31,7 +34,7 @@ model = Model(input=inputs, output=predictions)
 print('compile')
 model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
-              metrics=['accuracy'])
+              metrics=['accuracy', 'categorical_accuracy'])
 
 callbacks = [ModelCheckpoint("weights/weight{epoch:02d}-{val_loss:.2f}.hdf5", monitor='val_loss', verbose=0,
                            save_best_only=True, save_weights_only=True, mode='auto'),
